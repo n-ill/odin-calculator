1f28ffb84a22588cb209d30bc212a9bac90a104a
nill <naadir.iqbal1@gmail.com>
2021-12-22 18:41:00 -0600

    added functionality for add, subtract, and equals

---
+++ script.js
@@ -0,0 +1,40 @@
+const displayScreen = document.querySelector('.display-screen');
+
+const clearButton = document.querySelector('.button-clear');
+clearButton.addEventListener('click', () => { displayScreen.textContent = '' });
+
+const numberButtons = document.querySelectorAll('.button-number');
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        displayScreen.textContent += button.textContent;
+    })
+});
+
+let firstNum = 0;
+let operator = '';
+
+const equalButton = document.querySelector('.button-equal');
+equalButton.addEventListener('click', equals);
+
+const addButton = document.querySelector('.button-add');
+addButton.addEventListener('click', () => {
+    firstNum = parseInt(displayScreen.textContent);
+    operator = '+';
+    displayScreen.textContent = '';
+});
+
+const subButton = document.querySelector('.button-sub');
+subButton.addEventListener('click', () => {
+    firstNum = parseInt(displayScreen.textContent);
+    operator = '-';
+    displayScreen.textContent = '';
+});
+
+function equals() {
+    if (operator === '+') {
+        displayScreen.textContent = firstNum + parseInt(displayScreen.textContent);
+    }
+    if (operator === '-') {
+        displayScreen.textContent = firstNum - parseInt(displayScreen.textContent);
+    }
+}
